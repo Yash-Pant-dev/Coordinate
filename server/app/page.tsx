96954e8e71e2c468e1b73dcc4e79a17b014dbd52
Yash Pant <yashpantdev@gmail.com>
2024-06-30 23:02:08 +0530

    new start

---
+++ server/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return <>
+    Coordinate
+    <br />
+    A GUI based Work Orchestrator
+    <br />
+    <br />
+
+    <div className="flex flex-col">
+      <div>Start Building</div>
+      <div>Downloadables</div>
+      <div>About</div>
+    </div>
+  </>
+}
